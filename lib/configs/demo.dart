cb77a9120e56215a374284b7b62bb3c25f577970
Ahmad Rifa'i <ahmadrifai716@gmail.com>
2025-03-04 08:58:05 +0700

    var demo

---
+++ lib/configs/demo.dart
@@ -0,0 +1,18 @@
+List<String> banners = [
+  '/assets/images/banner1.jpeg',
+  '/assets/images/banner2.jpeg',
+  '/assets/images/banner3.jpeg',
+];
+
+List<Map<String, dynamic>> categories = [
+  {'name': 'Foods', 'icon': 'assets/icons/food.png', 'color': 0xFFf9a825},
+  {'name': 'Drinks', 'icon': 'assets/icons/drink.png', 'color': 0xFF4db6ac},
+  {'name': 'Snacks', 'icon': 'assets/icons/snack.png', 'color': 0xFFff7043},
+  {'name': 'Seafood', 'icon': 'assets/icons/seafood.png', 'color': 0xFF4db6ac},
+  {'name': 'Meat', 'icon': 'assets/icons/meat.png', 'color': 0xFFff7043},
+  {'name': 'Noodles', 'icon': 'assets/icons/noodle.png', 'color': 0xFF5c6bc0},
+  {'name': 'Rice', 'icon': 'assets/icons/rice.png', 'color': 0xFFf06292},
+  {'name': 'Soup', 'icon': 'assets/icons/soup.png', 'color': 0xFF66bb6a},
+  {'name': 'Bread', 'icon': 'assets/icons/bread.png', 'color': 0xFFf9a825},
+  {'name': 'Pizza', 'icon': 'assets/icons/pizza.png', 'color': 0xFF4db6ac},
+];
