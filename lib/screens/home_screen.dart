a201469fd1a53f2e8ef26902a5fc3b0ffd91edc3
Ahmad Rifa'i <ahmadrifai716@gmail.com>
2025-03-04 10:32:40 +0700

    home screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:praktikum_5/components/banner_carousel.dart';
+import 'package:praktikum_5/components/food_categories.dart';
+import 'package:praktikum_5/components/item_card_recipe.dart';
+import 'package:praktikum_5/components/title_section.dart';
+import 'package:praktikum_5/configs/demo.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        title: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Resep Makanan'.toUpperCase(),
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w700,
+                color: Colors.amber,
+              ),
+            ),
+            Text(
+              'Masak Sendiri Kesukaanmu',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+            ),
+          ],
+        ),
+        leading: Icon(Icons.menu),
+        actions: [
+          Container(
+            margin: EdgeInsets.only(right: 10),
+            child: Padding(
+              padding: EdgeInsets.all(6),
+              child: Icon(Icons.search),
+            ),
+          ),
+        ],
+      ),
+      body: CustomScrollView(
+        slivers: [
+          SliverPadding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            sliver: SliverToBoxAdapter(child: BannerCarousel()),
+          ),
+          TitleSection(title: "Jenis Makanan", onPressed: () {}),
+          SliverToBoxAdapter(child: FoodCategories()),
+          TitleSection(title: "Resep", onPressed: () {}),
+          // make list 2 grid column
+          SliverPadding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            sliver: SliverGrid(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 0,
+                childAspectRatio: 1,
+              ),
+              delegate: SliverChildBuilderDelegate(
+                // in each item give image and text below it
+                (context, index) => ItemCardRecipe(
+                  image: banners[index],
+                  title: "Resep Makanan ${index + 1}",
+                  onTap: () {},
+                ),
+                childCount: banners.length,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
